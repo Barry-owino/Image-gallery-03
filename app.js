18863a19a5b73a2b55fe2337bf5a3475e747daae
Barry-owino <barryowino5@gmail.com>
2022-05-01 02:30:21 +0300

    styled the container and the galleryImg class

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const gallery = document.getElementById('gallery');
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
-const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 910, 11, 12];
+const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const selectedIndex = null;
 
 imageIndexes.forEach((i) => {
   const image = document.createElement('img');
   image.src = `/images/barry-${i}.jpg`;
-  image.alt = `barry pic for ${i} of the image gallery`;
+  image.alt = `barry pic ${i} of the image gallery`;
   image.classList.add('galleryImg');
 
   image.addEventListener('click', () => {
